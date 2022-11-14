54571b2cf8f06fb07201a748fee12b6a13bee2cc
haimbacharr <81071510+haimbacharr@users.noreply.github.com>
2022-11-14 22:38:44 +0200

    initialize registeration variables and change id's names in register activity

--- app/src/main/java/com/example/ticket4u/Register.java
+++ app/src/main/java/com/example/ticket4u/Register.java
@@ -2,14 +2,36 @@ package com.example.ticket4u;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+
 public class Register extends AppCompatActivity {
+    EditText fullName,email,password,phone;
+    Button registerButton;
+    TextView alreadyLogin;
+    FirebaseAuth fireBaseAuth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        fullName = findViewById((R.id.fullName));
+        email = findViewById((R.id.email));
+        password = findViewById((R.id.password));
+        phone = findViewById((R.id.phone));
+        registerButton = findViewById((R.id.registerBtn));
+        alreadyLogin = findViewById(R.id.alreadyLogin);
+
+        fireBaseAuth = FirebaseAuth.getInstance();              /* get current instance of firebase */
+        progressBar = findViewById(R.id.progressBar);
+
     }
-    /////
+
 }
