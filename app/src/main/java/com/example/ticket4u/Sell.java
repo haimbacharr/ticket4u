09b02aeba5fa8e06f10f9cf0c1b2b48846259ada
eliranamerzoyev <eliranamerzoyev@gmail.com>
2022-11-22 15:31:30 +0200

    Add menu buttons, Add logo to pages, Change background, update app icon, Create Sell page.

---
+++ app/src/main/java/com/example/ticket4u/Sell.java
@@ -0,0 +1,13 @@
+package com.example.ticket4u;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sell extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sell);
+    }
+}
